cfb820fe4643572341df8b980d24be1a63a0eb82
Andrew <Andry.Sheremetew@gmail.com>
2024-08-13 15:43:57 +0300

    fix-1: fix login (replace Login js)

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -3,12 +3,10 @@ import './Login.css';
 import Card from './components/Card/Card';
 
 
-const login = () => {
-  return (
-    <div className='Login-page'>
-      <Card />
-      </div>
-  )
+const Login = () => {
+  <div className='Login-page'>
+    <Card />
+  </div>
 }
 
-export default login;
+export default Login;
