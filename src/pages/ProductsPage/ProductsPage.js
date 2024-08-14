2b00082fc4505da13d0e9e09965f22ed787ee41f
Andry <andry.sheremetew@gmail.com>
2024-08-14 04:05:13 +0300

    fix spinner and some bugs fix & replace pTable + pPage

--- src/pages/ProductsPage/ProductsPage.js
+++ src/pages/ProductsPage/ProductsPage.js
@@ -19,15 +19,32 @@ class ProductsPage extends Component {
   }
 
   componentDidMount(){
-    setTimeout(()=> ( this.getProducts()), 2000) }
+    setTimeout(()=> ( this.getProducts()), 2000) 
+  }
   
   async getProducts() {
-    const response = await fetch('https://66401c9ca7500fcf1a9d1857.mockapi.io/products');
-    const productsData = await response.json();
+    try {
+      const response = await fetch('https://66401c9ca7500fcf1a9d1857.mockapi.io/products');
 
+    if (!response.ok) {
+      throw new Error("Something went wrong");
+    }
+    
+    const productsData = await response.json();
+    
     this.setState({ products: productsData, })
+    this.setState({ isLoading: false });
     console.log(productsData);
   }
+
+  catch (error) {
+    this.setState({
+      isError: true,
+      isLoading: false,
+    });
+  }
+}
+
 
   render() {
 
@@ -38,10 +55,10 @@ class ProductsPage extends Component {
       <img className='ProductsLogo' alt='ProductsLogo' src={ProductsPageLogo} />
       <Button iconPreview iconAdd />
       <h1 className='productTitle'>Products</h1>
-      <ProductsTable products={products} isLoading={isLoading} isError={isError}/>
+      <ProductsTable products={products} isLoading={isLoading} isError={isError} />
     </div>
-    )
-  };
+    );
+  }
 }
 
 export default ProductsPage;
--- src/pages/ProductsPage/components/ProductsTable/ProductsTable.js
+++ src/pages/ProductsPage/components/ProductsTable/ProductsTable.js
@@ -17,22 +17,26 @@ class ProductsTable extends Component {
 
     return ( 
 
-      <div>
-
-      { isLoading ? (<img width="150px" height="150px" src={logo} className="App-logo" alt="logo" />) : (products.map((product) => (
-      <div className='ProductsTableContainer'>
-        <div className='productTableTitle'>
-          <p className='slotName'>ID{sortIcon}</p>
-          <p className='slotName'>Category{sortIcon}</p>
-          <p className='slotName'>Name{sortIcon}</p>
-          <p className='slotName'>Quantity{sortIcon}</p>
-          <p className='slotName'>Price($){sortIcon}</p>
-          <p className='slotName'></p>
+      <div className='ProductContainerMain'>
+        <div className='ProductsTableContainer'>
+          { isLoading ? (<img src={logo} className="App-logo" alt="spinner" />) : (
+                  <div className='productTableTitle'>
+                    <p className='slotName'>ID{sortIcon}</p>
+                    <p className='slotName'>Category{sortIcon}</p>
+                    <p className='slotName'>Name{sortIcon}</p>
+                    <p className='slotName'>Quantity{sortIcon}</p>
+                    <p className='slotName'>Price($){sortIcon}</p>
+                    <p className='slotName'></p>
+                  </div> 
+                )
+              }
         </div>
 
-        <div className='productTableField'>
+
+      { products.map((product) => (
+        <div className='productTableField' key={product.id}>
         <div className='productTableSelect'>
-          <div className='productItem' key={product.id}> 
+          <div className='productItem'> 
             <div className='productItem'>{product.id}</div> <div className='productItem'>{product.category}</div> 
               <div className='productItem' >{product.name}</div>  <div className='productItem' >{product.quantity}</div> 
               <div className='productItem' >{product.price}</div>
@@ -40,12 +44,11 @@ class ProductsTable extends Component {
                     <button className='buttonEdit'>{editIcon}</button>
                     <button className='buttonDelete'>{deleteIcon}</button></div>
                   </div>
+                </div>
               </div>
-          </div>
-          {isError && <p>Oops! sorry we have a problem...........?</p> }
-          </div>
-          )))}
-      </div>
+            ))}
+          {isError && <div className='errorContainer'><p className='errorText'>Oops! sorry we have a problem...</p></div>}
+        </div>
       )
     }
   }
