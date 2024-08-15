62a36e20d2330814e961a4ed63176c94018521ef
Andry <andry.sheremetew@gmail.com>
2024-08-15 23:14:33 +0300

    add functional for Pass Icon-eye

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,16 +1,28 @@
 
 import './Input.css'
 import { IoMdEye } from 'react-icons/io';
-import { IoMdEyeOff } from "react-icons/io"; 
+import { IoMdEyeOff } from "react-icons/io";
+import { useState } from 'react';
 
 
-const Input = ({  PasswordInput }) => {
+const Input = ({ PasswordInput }) => {
+
+  const [ isShowPass, setIsHidePass ] = useState(true);
+
+  const handleClick = () => {
+    setIsHidePass( prevIsShowPass => !prevIsShowPass );
+  }
+
   return (
-    <label className='loginLabel'>
+    <div className='InputContainer'>
       <input className='loginInput' type='text' placeholder='User Name' />
-      { PasswordInput && <input className='loginInput pass' type='password' placeholder='Password'/> } 
-      <button className='buttonEye'> <IoMdEye className='loginEyeIcon' size='30px' color='green' /> <IoMdEyeOff className='loginEyeIcon loginEyeHideIcon' size='30px' color='green' /> </button>
-    </label>
+      { PasswordInput && <input className='loginInput password' type='password' placeholder='Password'/>  }
+
+      <button onClick={handleClick} className='buttonEye'>
+        {isShowPass ? <IoMdEye size='30px' color='green' /> : 
+        <IoMdEyeOff size='30px' color='green' /> }
+      </button>
+    </div>
   );
 };
 
