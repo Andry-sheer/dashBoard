d90b79d7f8910725a1c8f3f1b1ed58bf8bc4e1a4
Andry <andry.sheremetew@gmail.com>
2024-08-31 00:55:30 +0300

    all fix is done-2

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,37 +3,4 @@ import "./Button.css";
 const Button = ({ className, type, icon, textButton, onClick }) => (
   <button type={type} className={className} onClick={onClick}>{icon}{textButton}</button> );
 
-
-
-
-
-// const Button = ({ Login, iconPreview, iconAdd }) => {
-//   return (
-//     <div className="ButtonContainer">
-//       {Login && (
-//         <button type="submit" className="Button">
-//           {" "}
-//           Login{" "}
-//         </button>
-//       )}
-
-//       <div className="buttonContainerPreview">
-//         {iconPreview && (
-//           <button type="button" className="Button two">
-//             <CgProfile className="iconButton" size="30px" /> Preview{" "}
-//           </button>
-//         )}
-//       </div>
-
-//       <div className="buttonContainerAdd">
-//         {iconAdd && (
-//           <button type="button" className="Button two">
-//             <IoMdAdd className="iconButton" size="30px" /> Add Product{" "}
-//           </button>
-//         )}
-//       </div>
-//     </div>
-//   );
-// };
-
 export default Button;
