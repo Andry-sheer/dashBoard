a428607907a0bdd32d9effa8012e23993cdb95b1
Andry <andry.sheremetew@gmail.com>
2024-09-26 02:40:18 +0300

    task is done! hw-31

---
+++ src/components/ModalWindows/ModalDelete.js
@@ -0,0 +1,69 @@
+
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
+import { connect } from 'react-redux';
+
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 600,
+  bgcolor: 'background.paper',
+  borderRadius: 12,
+  boxShadow: 24,
+  p: 5,
+  textAlign: 'center',
+};
+
+const textColor = {
+  color: '#05BC52',
+  fontWeight: 600,
+}
+
+const styleDelete = {
+  color: 'white',
+  fontWeight: 600,
+  padding: 10,
+  paddingLeft: 40,
+  paddingRight: 40,
+  background: 'red'
+}
+
+const styleCancel = {
+  color: 'white',
+  fontWeight: 600,
+  padding: 10,
+  paddingLeft: 40,
+  paddingRight: 40,
+  background: 'silver'
+}
+
+const ModalDelete =({open, product, onClose, onDeleteModal })=> {
+
+  return (
+    <div>
+      <Modal open={open}>
+          <Box sx={style}>
+            <div style={textColor} id="modal-modal-title" variant="h5" component="h2">
+              Are u sure you want to delete this product?
+            </div>
+
+            <div style={{display: 'flex', justifyContent:'space-around', marginTop: '30px'}} id="modal-modal-description" sx={{ mt: 5 }}>
+              <Button onClick={onClose} style={styleCancel}>Cancel</Button>
+              <Button onClick={()=> onDeleteModal(product)} style={styleDelete}>Delete</Button>
+            </div>
+          </Box>
+      </Modal>
+    </div>
+    
+  );
+}
+
+const mapStateToProps = (state) => ({
+  products: state.products.productsData,
+});
+
+export default connect(mapStateToProps)(ModalDelete);
--- src/pages/ProductsPage/ProductsPage.js
+++ src/pages/ProductsPage/ProductsPage.js
@@ -3,56 +3,84 @@ import { useState, useEffect } from "react";
 import { API_URL } from "../../constants/constants";
 import "./ProductsPage.css";
 import ProductsPageLogo from "../../assets/pagesLogo.svg";
-import Button from "../../components/Button/Button";
+import Buttons from "../../components/Button/Button";
 import { IoMdAdd } from "react-icons/io";
 import { CgProfile } from "react-icons/cg";
 import ProductsTable from "../ProductsPage/components/ProductsTable/ProductsTable";
 import { useNavigate } from "react-router-dom";
 import { addProducts, setIsError } from "../../modules/actions/products";
 import { connect } from "react-redux";
+import ModalDelete from "../../components/ModalWindows/ModalDelete";
+
 
 const ProductsPage = ( { addProducts, setIsError } ) => {
-
   const navigatePreview = useNavigate();
+  const [isLoadProducts, setIsLoadProducts] = useState(false);
+  const [isOpenModal, setIsOpenModal] = useState(false);
+  const [productDelete, setProductDelete] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
 
   const handleButtonPreview = () => {
     navigatePreview("/preview-page")
   }
 
-  const [isLoading, setIsLoading] = useState(true);
-
   useEffect(() => {
+    if (!isLoadProducts){
       getProducts();
-  }, []);
+    }}, [isLoadProducts]);
+
 
   const getProducts = async () => {
     try {
-      const response = await fetch(API_URL + "products");
-
+      const response = await fetch(`${API_URL}/products`);
       if (!response.ok) {
         throw new Error("Something Error");
       }
 
       const productsData = await response.json();
       addProducts(productsData);
-      setIsLoading(false); // true         
+      setIsLoading(false); // true
+      setIsLoadProducts(true);
+
     } catch (error) {
       setIsError(true);
       setIsLoading(false);
     }
   };
 
+  const handleOpenModal = (product) => {
+    setProductDelete(product);
+    setIsOpenModal(true);
+  };
+
+  const handleCloseModal =()=> {
+    setIsOpenModal(false);
+    setProductDelete(null);
+  }
+
+  const deleteProducts = async () => {
+    if (!productDelete) return;
+
+    await fetch(`${API_URL}/products/${productDelete.id}`, {
+      method: 'DELETE'
+    });
+
+    setIsLoadProducts(false);
+    setIsOpenModal(false);
+  }
+
 
   return (
     <div className="ProductsPage">
       <img className="ProductsLogo" alt="ProductsLogo" src={ProductsPageLogo} />
-      <Button onClick={handleButtonPreview} type='button' className='buttonPreview' textButton='Preview' icon={<div className="iconPreview"><CgProfile/></div>} />
-      <Button type='button' className='buttonAdd' textButton='Add Product' icon={<div className="iconAdd"><IoMdAdd/></div>} />
+      <Buttons onClick={handleButtonPreview} type='button' className='buttonPreview' textButton='Preview' icon={<div className="iconPreview"><CgProfile/></div>} />
+      <Buttons type='button' className='buttonAdd' textButton='Add Product' icon={<div className="iconAdd"><IoMdAdd/></div>} />
       <h1 className="productTitle">Products</h1>
 
-      <ProductsTable
-        isLoading={isLoading}
-      />
+      <ProductsTable isLoading={isLoading} onDeleteModal={handleOpenModal}/>
+
+      <ModalDelete open={isOpenModal} onClose={handleCloseModal} onDeleteModal={deleteProducts} />
 
     </div>
   );
